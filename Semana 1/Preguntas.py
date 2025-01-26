a922ce46d7f556629684e01c891b4d183ccc4127
Edson Azuara <edazuaral007@gmail.com>
2025-01-26 16:54:40 -0600

    Preguntas

--- Semana 1/Preguntas.py
+++ Semana 1/Preguntas.py
@@ -9,19 +9,19 @@
 #Pregunta 2
 
 # ¿Qué es un programa?
-""""R"""""
+""""R: Conjunto de intrucciones que se utlizan para realizar una determinada serie de acciones."""""
 
 #Ejercicio 3
 
 # ¿Cuál es la diferencia entre un compilador y un intérprete?
-"""R"""
+"""El interprete lee el código fuente del programa como ha sido escrito, lo analiza e interpreta sus instruccines sobre la marcha, mientras que el compilador traduce el código fuente de alto nivel a código máquina, colocandolo dentro de otro fichero."""
 
 #Ejercicio 4
 
 # ¿Cuál de los siguientes contiene “código máquina”?
 # a) El intérprete de Python
 # b) El teclado
-# c) El código fuente de Python
+""" c) El código fuente de Python"""
 # d) Un documento de un procesador de texto
 
 #Ejercicio 5
@@ -34,7 +34,8 @@
 #SyntaxError: invalid syntax
 #>>>
 
-"""R"""
+"""Un error de sintaxis, ya que no ha seguido las reglas gramaticales de pyhton, en todo caso sería:"""
+"""print("Hola mundo")"""
 
 #Ejercico 6
 
@@ -42,7 +43,7 @@
 # X = 123
 
 # a) Unidad central de procesamiento
-# b) Memoria Principal
+""" b) Memoria Principal"""
 # c) Memoria Secundaria
 # d) Dispositivos de Entrada
 # e) Dispositivos de Salida
@@ -54,8 +55,12 @@
 #x = x + 1
 #print(x)
 
+x = 43
+x = x + 1
+print(x)
+
 # a) 43
-# b) 44
+"""# b) 44"""
 # c) x + 1
 # d) Error, porque x = x + 1 no es posible matemáticamente
 
@@ -64,8 +69,14 @@
 #Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana: (1) Unidad central de procesamiento, (2) Memoria principal, (3) Memoria secundaria, (4) Dispositivos de entrada, y (5) Dispositivos de salida. Por ejemplo, “¿Cuál sería el equivalente humano de la Unidad central de
 #procesamiento?”.
 
+"""El CPU sería como la parte del cerebro encargada de la inteligencia, tomando decisiones rápidamente y encargada del pensamiento."""
+"""La memoria principal sería como la visión y la memoria a corto plazo, por ejemplo antes de presentar un examen, habiendo estudado el día anterior."""
+"""La memoria secundaria sería como la memoria a largo plazo, aquella encargada de los recuerdos y de los conociemtos adquiridos."""
+"""Los dispositovos de entrada serían como nuestros sentidos los cuales reiben información del entorno o bien como la boca encargada de la alimentación(recarga)"""
+"""Los dispositovos de salida serían más o menos lo mismo que los de entrada por donde sale y entra la información."""
+
 #Ejercicio 9
 
 #¿Cómo puedes corregir un “Error de sintaxis”?.
 
-"""R"""
+"""Revisando el punto de partida de donde pyhton lo detecto y revisar si donde se ecnuentra este."""
