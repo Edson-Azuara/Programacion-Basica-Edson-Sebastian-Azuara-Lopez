ea03e8feb5d300ba38d986781fff0b8c26e90973
Edson Azuara <edazuaral007@gmail.com>
2025-01-25 23:37:04 -0600

    preguntas

--- Semana 1/Preguntas.py
+++ Semana 1/Preguntas.py
@@ -1,3 +1,7 @@
-print("Hola mundo")
-print("Usted debe de ser el dios legendario que viene del cielo")
-print("Hemos estado esperandolo durante mucho tiempo")
+"""
+# ¿Cual es la función de la memoria secundaria en una computadora?
+# a) Ejecutar todos los cálculos y la lógica del programa
+# b) Descargar páginas web de Internet
+# c) Almacenar información durante mucho tiempo, incluso después de ciclos de
+apagado y encendido
+# d) Recolectar la entrada del usuario"""
