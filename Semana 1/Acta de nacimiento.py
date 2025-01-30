9153546d564f9096522776dae52163b1435ace4b
Edson Azuara <edazuaral007@gmail.com>
2025-01-29 21:10:45 -0600

    Acta de nacimiento

---
+++ Semana 1/Acta de nacimiento.py
@@ -0,0 +1,16 @@
+#Acta de nacimiento 
+
+Numero = float
+
+Lista = list()
+Conjunto = set()
+
+Lista = ["Nombre", "FechaNac", "LugarNac"]
+Datos = ["Edson Sebastián Azuara López", "7/11/2007"]
+
+ESAL = [Lista, Datos]
+
+Numero = {}
+Numero = ESAL
+
+print(Numero)
