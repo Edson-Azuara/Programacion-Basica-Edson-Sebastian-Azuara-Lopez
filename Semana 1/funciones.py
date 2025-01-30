a96f136e1989ad1b04db34c364286ec6ff38a1b4
Edson Azuara <edazuaral007@gmail.com>
2025-01-29 23:23:04 -0600

    funciones

---
+++ Semana 1/funciones.py
@@ -0,0 +1,14 @@
+#funciones
+
+def nombrefuncion():
+    print("hola mundo desde función")
+
+def suma (a, b):
+    return a + b
+def resta (a, b):
+    return a - b
+
+nombrefuncion()
+print(suma(5,7))
+
+print(resta(3,1))
