2c5a2099c48b74fd8d1be9e1337a13164c0e9126
Edson Azuara <edazuaral007@gmail.com>
2025-02-06 22:30:15 -0600

    pregunta 10

---
+++ 100 preguntas/Preguntas 10-20/Pregunta 10.py
@@ -0,0 +1,8 @@
+#Pregunta 10
+
+variable2 = open("pregunta 10 archivo.txt","w")
+
+#Si se hace con el método 2, es necesario cerrar la variable
+print("Hola mundo desde North Korea", file=variable2)
+
+variable2.close()
